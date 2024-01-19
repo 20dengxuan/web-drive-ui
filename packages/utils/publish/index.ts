d5fe7b3d18a8df159b118c68c50c55b0f87c40ce
邓玄 <dengxuan@d.local>
2024-01-19 10:22:10 +0800

    feat: utilsa

--- packages/utils/publish/index.ts
+++ packages/utils/publish/index.ts
@@ -1,6 +1,7 @@
 import run from '../../components/script/utils/run';
 import { series } from 'gulp';
+import { pkgPath } from "../../components/script/utils/paths";
 export const publishComponent = async () => {
-  run('release-it', `./`);
+  run('release-it', `${pkgPath}/utils`);
 };
 export default series(async () => publishComponent());
