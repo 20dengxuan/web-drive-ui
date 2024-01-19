25cf5a093f08c66c73b0db7b87130f461cbccc1f
邓玄 <dengxuan@d.local>
2024-01-19 09:42:07 +0800

    feat: utils

--- packages/components/src/button/index.ts
+++ packages/components/src/button/index.ts
@@ -1,5 +1,5 @@
 import _Button from "./button.vue";
-import { withInstall } from '@web-drive-ui/utils';
+import { withInstall } from 'web-drive-utils';
 
 export const Button = withInstall(_Button);
 export default Button;
